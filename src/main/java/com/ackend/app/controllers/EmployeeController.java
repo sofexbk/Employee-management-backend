9d02489a9c6d87f8e00984ada05bbb8efae7120e
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-09-14 17:15:10 +0100

    delete

--- src/main/java/com/ackend/app/controllers/EmployeeController.java
+++ src/main/java/com/ackend/app/controllers/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.ackend.app.controllers;
 
+import java.util.HashMap;
 import java.util.List;
-
+import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,10 +13,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.ackend.app.exceptions.ResourceNotFoundException;
 import com.ackend.app.models.Employee;
 import com.ackend.app.repository.EmployeeRepository;
+
+
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -35,7 +38,7 @@ public class EmployeeController {
     return employeeRepository.save(employee);
    }
 
-   
+
    //get employee by id rest Api
    @GetMapping("/employees/{id}")
    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
@@ -53,11 +56,21 @@ public class EmployeeController {
      employee.setFirstName(employeeDetails.getFirstName());
      employee.setLastName(employeeDetails.getLastName());
      employee.setEmailId(employeeDetails.getEmailId()); 
-     
      Employee updatedEmployee=employeeRepository.save(employee);
      return ResponseEntity.ok(updatedEmployee);
-
    }
+
+   //delete employee rest Api
+    @DeleteMapping("/employees/{id}")
+     public ResponseEntity<java.util.Map<String, Boolean>> deleteEmployee (@PathVariable Long id){
+      Employee employee = employeeRepository.findById(id)
+           .orElseThrow(()->new ResourceNotFoundException("Employee doesnt exist with id :"+id));
+
+        employeeRepository.delete(employee);
+        java.util.Map<String, Boolean> response = new java.util.HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+     }
 
 
 
