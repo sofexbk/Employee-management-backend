6c424c4de449a5384fee61205b9af9aef54ae0cd
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-09-14 16:37:04 +0100

    get 1

--- src/main/java/com/ackend/app/controllers/EmployeeController.java
+++ src/main/java/com/ackend/app/controllers/EmployeeController.java
@@ -3,12 +3,15 @@ package com.ackend.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ackend.app.exceptions.ResourceNotFoundException;
 import com.ackend.app.models.Employee;
 import com.ackend.app.repository.EmployeeRepository;
 
@@ -30,4 +33,16 @@ public class EmployeeController {
    public Employee createEmployee(@RequestBody Employee employee){
     return employeeRepository.save(employee);
    }
+
+   //get employee by id rest Api
+   @GetMapping("/employees/{id}")
+   public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+    Employee employee = employeeRepository.findById(id)
+       .orElseThrow(()->new ResourceNotFoundException("Employee doesnt exist with id :"+id));
+        return ResponseEntity.ok(employee);
+   }
+
+
+
+
 }
