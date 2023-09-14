05863795c0e8fcaa2a2c91e474427d8230f44c59
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-09-14 11:09:07 +0100

    first

---
+++ src/main/java/com/ackend/app/models/Employee.java
@@ -0,0 +1,5 @@
+package com.ackend.app.models;
+
+public enum Employee {
+    
+}
---
+++ src/test/java/com/ackend/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ackend.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
